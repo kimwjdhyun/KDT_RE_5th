90de9d8d794a16794b52304cce373e9d992ae855
kimwjdhyun <dkrehd7953@gmail.com>
2026-01-26 14:01:59 +0900

    아두이노:파이썬 없이 html, 시리얼 연결

--- 5_Arduino/15_Serial_01/15_Serial_02.py
+++ 5_Arduino/15_Serial_01/15_Serial_02.py
@@ -34,5 +34,4 @@ def led():
     # 예를 들어서 desred == "on"일때는 jsonify({"전송" : False, "state" : state})
 
 if __name__ == "__main__":
-    app.run(host = "0, 0, 0, 0", port = 5000)
-    
+    app.run(host = "0.0.0.0", port = 5000)
