3ddf52c4e4d7e219abc809d65cdd938372b28c21
kimwjdhyun <dkrehd7953@gmail.com>
2026-01-28 12:48:29 +0900

    javascipt:형변환 학습

--- 4_Web/03_JavaScript/03_data-type.js
+++ 4_Web/03_JavaScript/03_data-type.js
@@ -163,3 +163,70 @@ console.log(typeof undefined); //undefined
 console.log(typeof fruits); // object
 console.log(typeof cat) // object
 
+// ==================================================
+
+// 형변환
+
+// 1. 암시적 형변환
+console.log("암시적 형변환 1", "2" + 3);
+// 암시적 형변환 (1), 2 3
+// 기대 : 암시적 형변환 (1), 2(문자열), 3(숫자형)
+// 결과 : 암시적 형변환 (1), 2(문자열), 3(문자열)
+
+
+console.log("암시적 형변환 (2)", typeof (100 + '1'));
+// 기대 : 에러나야함
+// 숫자 더하기 문자는 타입을 알 수 없음
+// 결과 : 암시적 형변환 (2) string
+// JS가 자기 마음대로 숫자를 문자로 변환해버림
+
+// 2. 암시적 형변환
+
+// 2-1. 문자열로 변환 : string(), tostring()
+
+let string1 = 123;
+let string2 = true;
+let string3 = undefined;
+
+// String() 사용
+console.log(String(string1), typeof String(string1)); // 숫자 123은 문자열 ["123"]으로 변환
+console.log(String(string2), typeof String(string2)); // "true" 문자열
+console.log(String(string3), typeof String(string3)); // "undefined" 문자열
+
+// .tostring() 사용
+
+console.log(string1.toString());
+console.log(string2.toString());
+// console.log(string3.toString()); // 에러 발생 : 변수 값이 없기 때문에 변환 불가
+
+// 2-2. 숫자로 변환 : Number(), parseInt()
+
+let number1 = '123';
+let number2 = false;
+let number3 = "진짜 문자열";
+let number4 = 3.14;
+let number5 = "3.14";
+
+//Number() 사용
+console.log("=====================================");
+console.log(Number(number1), typeof Number(number1)); // 123
+console.log(Number(number2), typeof Number(number2)); // 0
+console.log(Number(number3), typeof Number(number3)); // NaN ( Not a Number)
+console.log(Number(number4), typeof Number(number4)); // 3.14
+console.log(Number(number5), typeof Number(number5)); // 3.14
+
+// "정의되지 않음" => 값이 아예 없는 것 => NaN
+console.log(Number(undefined), typeof Number(undefined)); // NaN
+// "값이 없음을 정의" => 값이 없다는 것을 표현하는 값인 0으로 변환
+console.log(Number(null), typeof Number(null)); // 0
+
+// parseInt() 사용 => 소수점 버리고 정수로 출력
+console.log("=====================================");
+console.log(parseInt(number1), typeof parseInt(number1)); // 123
+console.log(parseInt(number4), typeof parseInt(number4)); // 3
+console.log(parseInt(number5), typeof parseInt(number5)); // 3
+
+// parseFloat() 사용 => 소수점까지 모두 변환, 자료형은 number로 출력
+console.log(parseFloat(number4), typeof parseFloat(number4)); // 3.14
+console.log(parseFloat(number5), typeof parseFloat(number5)); // 3.14
+
--- 4_Web/03_JavaScript/03_datatype_practice.js
+++ 4_Web/03_JavaScript/03_datatype_practice.js
@@ -1,9 +1,24 @@
 let pansoli = {
     name : "이몽룡",
     age : 18,
+    like : ["강아지", "고양이"],
     isMarried: true,
     girlfriend: {name: '성춘향', age: 16}
 };
 
 console.log("춘향전", pansoli);
 
+let mathScore = "77";
+let engScore = "88";
+
+let avgScore = (Number(mathScore) + Number(engScore)) / 2;
+
+console.log("수학 점수", mathScore.toString(), "점");
+console.log("영어 점수", engScore.toString(), "점");
+console.log("평균 " + avgScore + "점")
+
+
+let mathScore1 = prompt('수학점수 입력');
+let engScore1 = prompt('영어점수 입력');
+let avgScore1 = (Number(mathScore1) + Number(engScore1)) / 2;
+alert(`평균 점수 ${avgScore1}점 입니다.`)
