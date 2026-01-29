e74b9d82cc20d7cb64d7a15be1338647a32fff8a
kimwjdhyun <dkrehd7953@gmail.com>
2026-01-29 09:12:52 +0900

    javasript 파일정리

---
+++ 4_Web/03_JavaScript/05_func_practice.js
@@ -0,0 +1,14 @@
+function multyfly(x, y) {
+    x = Number(x);
+    y = Number(y);
+    return x * y;
+}
+
+console.log(multyfly(3, 7));
+
+let square = function(x) {
+    x = Number(x);
+    return x ** 2;
+}
+
+console.log(square(50));
