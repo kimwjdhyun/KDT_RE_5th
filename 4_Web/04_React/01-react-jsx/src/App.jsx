970383228c2d14beec71da842efcf59ac89ae5b7
kimwjdhyun <dkrehd7953@gmail.com>
2026-02-03 14:12:29 +0900

    react : react 설치, 기초, component, JSX 학습

---
+++ 4_Web/04_React/01-react-jsx/src/App.jsx
@@ -0,0 +1,79 @@
+import logo from './logo.svg';
+import './App.css';
+import './app1.css'
+
+function App() {
+  const name = 'kimwjdhyun';
+  const styles = {
+    backgroundColor :'yellow',
+    color : 'pink',
+    fontSize : '48px'
+  }
+  const name1 = '호굼';
+  const animal = '강아지';
+  const a = 17;
+  const b = 13;
+  const title = '안녕하세요. 반갑습니다!'
+  return (
+    <>
+      <div className="App">
+        {/* JSX 문법 */}
+        {/* 1. 하나로 감싸인 요소 */}
+        <div>반가워요!</div>
+        {/* 
+          2. javascript 표현식 사용
+          - {}로 감싸면 js 표현식 사용 가능
+          - {} 안에서 삼항 연산자 사용 가능 (if, for 문 사용 x) 
+        */}
+        <div>{name} 안녕하세요</div>
+        <div>{name === 'kimwjdhyun' ? '반갑습니다.' : '누구세요?'}</div>
+
+        {/* 
+          3. style 속성
+          - 리액트에서 dom 요소에 스타일 적용 시 문자열 x -> 객체 사용
+          - 스타일 이름 중 하이픈(-) 포함 시 camelCase로 작성해야 함
+        */}
+        <div style={styles}>하이</div>
+        <div style={{
+          backgroundColor :'yellow',
+          color : 'pink',
+          fontSize : '48px'
+        }}>헬로</div>
+
+        {/* 
+          4. className 사용
+          - class 속성이 아닌 className을 사용
+          
+          5. 종료 태그가 없는 태그의 사용
+          - 기존의 종료 태그가 없느 ㄴ태그를 사용하더라도 종료 태그를 작성해야함 or self-closing(/)
+          
+          6. 주석
+          - // : jsx외부 파일의 주석
+        */}
+          <h2 style={{textAlign:'left'}}>
+            제 반려 동물의 이름은 <u>{name1}</u>입니다.
+          <br />
+          동물은 <u>{animal}</u>입니다.
+          </h2>
+          <div>
+            {3 + 5 == 8 ? '정답입니다!' : '오답입니다!'}
+          </div>
+          <div>
+            {a > b && 'a가 b보다 큽니다' }
+          </div>
+          <div className='main'>
+            <h1>{title}</h1>
+            <div>
+                아이디 : <input type='text' />
+            </div>
+            <div>
+                비밀번호 : <input type='password' />
+            </div>
+          </div>
+      </div>
+    <div></div>
+    </>
+  );
+}
+
+export default App;
