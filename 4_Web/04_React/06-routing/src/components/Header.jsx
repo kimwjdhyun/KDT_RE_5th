914a2c0041672d2b650acd0ab8fd8b77e523586b
kimwjdhyun <dkrehd7953@gmail.com>
2026-02-06 14:28:42 +0900

    react : Routing학습

---
+++ 4_Web/04_React/06-routing/src/components/Header.jsx
@@ -0,0 +1,13 @@
+function Header() {
+    return (
+        <Header>
+            <span>Router Tutorial</span>
+            <ul>
+                <li>Home</li>
+                <li>Product</li>
+            </ul>
+        </Header>
+    );
+}
+
+export default Header;
