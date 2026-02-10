71a8b223676c73e91174f51da13b9b82dc1ebeee
kimwjdhyun <dkrehd7953@gmail.com>
2026-02-10 14:34:53 +0900

    opencv:비디오 저장 하기

--- 6_OpenCV/03_Webcam.py
+++ 6_OpenCV/03_Webcam.py
@@ -2,27 +2,27 @@ import cv2
 
 # 장치의 0번째 카메라를 불러오기
 
-# cap = cv2.VideoCapture(0)
+cap = cv2.VideoCapture(0)
 
-# if not cap.isOpened(): # 카메라가 정상적으로 열리지 않을 경우
-#     print("카메라가 없어요")
-#     exit()
+if not cap.isOpened(): # 카메라가 정상적으로 열리지 않을 경우
+    print("카메라가 없어요")
+    exit()
 
-# # 카메라 사진 찍기
-# while cap.isOpened():
-#     ret, img = cap.read()
+# 카메라 사진 찍기
+while cap.isOpened():
+    ret, img = cap.read()
 
-#     if ret:
-#         cv2.imshow('Camera', img)
+    if ret:
+        cv2.imshow('Camera', img)
 
-#         # 10ms 동안 키 입력을 대기
-#         # 키가 입력되면 (-1이 아니면) 사진을 저장하고 종료
-#         if cv2.waitKey(10) != -1:
-#             cv2.imwrite("6_OpenCV/output/capture.jpg", img)
-#             break
+        # 10ms 동안 키 입력을 대기
+        # 키가 입력되면 (-1이 아니면) 사진을 저장하고 종료
+        if cv2.waitKey(10) != -1:
+            cv2.imwrite("6_OpenCV/output/capture.jpg", img)
+            break
 
-# cap.release()
-# cv2.destroyAllWindows()
+cap.release()
+cv2.destroyAllWindows()
 
 practice = cv2.VideoCapture(0)
 practice.set(cv2.CAP_PROP_FRAME_WIDTH, 1200)
